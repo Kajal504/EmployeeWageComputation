23b7e793678dd5168f0d51649b8ac5ef617c33c0
KajalShendage <shendagekajal123@gmail.com>
2022-06-18 10:25:28 +0530

    UC2: Calculate emp Daily WAge

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,12 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage");
+int IS_FULL_TIME = 1;
+int EMP_RATE_PER_HOUR = 20;
+int empHrs = 0;
+int empWage = 0;
 Random obj = new Random();
 int num = obj.Next(0, 2);
-if (num == 1)
+if (num == IS_FULL_TIME)
 {
-    Console.WriteLine("Employee is Present");
+    empHrs = 8;
 }
 else
 {
-    Console.WriteLine("Employee is Absent");
+    empHrs = 0;
 }
+empWage = empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage : " + empWage);
+Console.ReadKey();
+        
