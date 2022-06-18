bfac46e2803e2fa05a6a29a3b2d28670d5df8516
KajalShendage <shendagekajal123@gmail.com>
2022-06-18 10:56:13 +0530

    UC5:cal working days

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -3,25 +3,32 @@ Console.WriteLine("Welcome to Employee Wage");
 const int IS_PART_TIME = 1;
 const int IS_FULL_TIME = 2;
 const int EMP_RATE_PER_HOUR = 20;
+const int NUM_OF_WORKING_DAYS = 20;
 int empHrs = 0;
 int empWage = 0;
-Random obj = new Random();
-int num = obj.Next(0, 3);
-switch (num)
+int totalempwage = 0;
+for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
 {
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-    case IS_PART_TIME:
-        empHrs = 4;
-        break;
-    default:
-        empHrs = 0;
-        break;
+    Random obj = new Random();
+    int num = obj.Next(0, 3);
+    switch (num)
+    {
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
+        case IS_PART_TIME:
+            empHrs = 4;
+            break;
+        default:
+            empHrs = 0;
+            break;
+    }
+    empWage = empHrs * EMP_RATE_PER_HOUR;
+    totalempwage += empWage;
+    Console.WriteLine("Emp Wage = " + empWage);
 }
-empWage = empHrs * EMP_RATE_PER_HOUR;
-Console.WriteLine("Employee wage : " + empWage);
-        
+Console.WriteLine("Total Emp Wage = " + totalempwage);
+             
     
 
 
