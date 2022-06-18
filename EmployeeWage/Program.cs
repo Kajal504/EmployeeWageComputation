efba6ae243295e27e1614c83b176a86016d5d25c
KajalShendage <shendagekajal123@gmail.com>
2022-06-18 10:07:03 +0530

    UC1:check for Emp Pre or Ab

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Employee Wage");
+Random obj = new Random();
+int num = obj.Next(0, 2);
+if (num == 1)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
