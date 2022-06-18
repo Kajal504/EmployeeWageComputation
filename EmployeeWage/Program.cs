c7a8c5bdf62746058e3c725cceb11c14b7a83015
KajalShendage <shendagekajal123@gmail.com>
2022-06-18 10:35:28 +0530

    UC3:Cal part time emp

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,14 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage");
-int IS_FULL_TIME = 1;
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
 int empWage = 0;
 Random obj = new Random();
-int num = obj.Next(0, 2);
+int num = obj.Next(0, 3);
 if (num == IS_FULL_TIME)
 {
     empHrs = 8;
+}
+else if (num == IS_PART_TIME)
+{
+    empHrs = 4;
 }
 else
 {
@@ -16,5 +21,9 @@ else
 }
 empWage = empHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Emp Wage : " + empWage);
-Console.ReadKey();
         
+    
+
+
+
+
